88fc17edd78fd35f187958d02745cbd25089c65e
charleslong13 <charleslong13@icloud.com>
2021-10-20 15:02:08 -0500

    finished Kneel Diamond project for review

---
+++ scripts/KneelDiamonds.js
@@ -0,0 +1,58 @@
+
+import { DiamondSizes } from "./DiamondSizes.js"
+import { JewelryStyles } from "./JewelryStyles.js"
+import { Orders } from "./Orders.js"
+import {Metals} from "./Metals.js"
+import {addCustomOrder} from "./database.js"
+
+let theDiamondSizes = DiamondSizes()
+console.log(theDiamondSizes)
+
+// let theJewelryStyles = JewelryStyles()
+// console.log(theJewelryStyles)
+
+let customerOrders = Orders()
+console.log(customerOrders)
+
+
+//add event listner function below 
+document.addEventListener(
+    "click",
+    (event) => {
+        if (event.target.id === "orderButton") {
+            addCustomOrder()
+        }
+    }
+)
+
+export const KneelDiamonds = () => {
+    return `
+        <h1>Kneel Diamonds</h1>
+
+        <article class="choices">
+            <section class="choices__metals options">
+                <h2>Metals</h2>
+                ${Metals()}
+            </section>
+            <section class="choices__sizes options">
+                <h2>Sizes</h2>
+                ${DiamondSizes()}
+            </section>
+            <section class="choices__styles options">
+                <h2>Styles</h2>
+                ${JewelryStyles()}
+            </section>
+        </article>
+
+        <article>
+            <button id="orderButton">Create Custom Order</button>
+            
+        </article>
+
+        <article class="customOrders">
+            <h2>Custom Jewelry Orders</h2>
+            ${Orders()}
+        </article>
+    `
+}
+
